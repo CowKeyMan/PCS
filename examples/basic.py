f00f5aea7ba8e52d995875a34e822b21653410b5
CowKeyMan <dancauchi1@gmail.com>
2025-04-13 08:34:11 +0300

    Add method to convert a component back to a dictionary

--- examples/basic.py
+++ examples/basic.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
-from pcs.init import initialize_object_nones
+
 from pcs.argument_parser import parse_arguments
+from pcs.component import get_non_null_members_as_dict
+from pcs.init import initialize_object_nones
 from pcs.pipeline import Pipeline
 
 
@@ -9,6 +11,7 @@ class Component:
     i: int
     f: float
     s: str
+    s2: str
     result: float
 
 
@@ -28,13 +31,26 @@ def print_string(s: str):
     print("s:", s)
 
 
+def print_string2(s2: str):
+    print("s2:", s2)
+
+
 component = initialize_object_nones(Component)
 parse_arguments(component)
 
 pipeline = Pipeline(
     component,
-    [print_add_system, print_result, result_add_system, print_result, print_string],
+    [
+        print_add_system,
+        print_result,
+        result_add_system,
+        print_result,
+        print_string,
+        print_string2,
+    ],
 )
 
 pipeline.execute()
 pipeline.execute()
+
+print(get_non_null_members_as_dict(component))
