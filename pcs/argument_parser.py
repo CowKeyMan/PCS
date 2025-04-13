06d3e90b2ac3f0848748bd15b9fc94ffd14861e9
CowKeyMan <dancauchi1@gmail.com>
2025-04-13 08:40:16 +0300

    Parse arguments now returns the dictionary

--- pcs/argument_parser.py
+++ pcs/argument_parser.py
@@ -1,5 +1,6 @@
 import oyaml as yaml
 import argparse
+from typing import Any
 
 
 class Args(argparse.Namespace):
@@ -14,11 +15,12 @@ def parse_arguments_from_files(obj: object, args_files: list[str]) -> None:
     update_object(obj, update_dict)
 
 
-def parse_arguments(obj: object) -> None:
+def parse_arguments(obj: object) -> dict[str, Any]:
     args = do_parse_arguments()
     update_dict = get_update_dict_from_comma_separated_file_list(args.args_files)
     update_dict_with_rest_arguments(update_dict, args.rest)
     update_object(obj, update_dict)
+    return update_dict
 
 
 def do_parse_arguments() -> Args:
