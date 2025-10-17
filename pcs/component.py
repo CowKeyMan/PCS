c4ed212bc37f6aabc210660d8a0fc30b9c855fec
Daniel Cauchi <33454325+CowKeyMan@users.noreply.github.com>
2025-10-17 11:15:18 +0300

    Allow component class to be serialized

--- pcs/component.py
+++ pcs/component.py
@@ -1,7 +1,6 @@
 from typing import Generic, TypeVar
 
 from omegaconf import DictConfig, OmegaConf
-
 from pcs.init import initialize_object_nones
 
 T = TypeVar("T")
@@ -17,9 +16,9 @@ class Component(Generic[T]):
 
     def __init__(self, conf: DictConfig, runtime: T):
         duplicate_keys = set(conf.keys()).intersection(set(runtime.__dict__))
-        assert (
-            len(duplicate_keys) == 0
-        ), f"Error initializing component - duplicate keys found: {duplicate_keys}"
+        assert len(duplicate_keys) == 0, (
+            f"Error initializing component - duplicate keys found: {duplicate_keys}"
+        )
 
         super().__setattr__("conf", conf)
         super().__setattr__("runtime", runtime)
@@ -39,6 +38,8 @@ class Component(Generic[T]):
         return super().__getattribute__("runtime")
 
     def __getattribute__(self, name):
+        if name not in ["conf", "runtime"]:
+            return super().__getattribute__(name)
         conf: DictConfig = super().__getattribute__("conf")
         if name in conf.keys():
             if name in conf:
@@ -47,28 +48,26 @@ class Component(Generic[T]):
         runtime: T = super().__getattribute__("runtime")
         if hasattr(runtime, name):
             return getattr(runtime, name)
-        return super().__getattribute__(name)
 
-    def __setattr__(self, name, item):
+    def __setattr__(self, name: str, item):
+        if name in ["conf", "runtime"]:
+            return super().__setattr__(name, item)
         conf: DictConfig = super().__getattribute__("conf")
         if name in conf.keys():
             return setattr(conf, name, item)
         runtime: T = super().__getattribute__("runtime")
         if hasattr(runtime, name):
             return setattr(runtime, name, item)
-        if name == "conf":
-            return super().__setattr__("conf", conf)
-        if name == "runtime":
-            return super().__setattr__("runtime", runtime)
         raise AttributeError(f"{name} not found in class Component")
 
     def __hasattr__(self, name):
-        return hasattr(self.runtime, name) or hasattr(self.conf, name)
+        return super().__hasattr__(name) or hasattr(self.runtime, name) or hasattr(self.conf, name)
 
     def __repr__(self):
         conf = super().__getattribute__("conf")
         runtime = super().__getattribute__("runtime")
-        return f"Component({conf=}, {runtime=})"
+        sealed = super().__getattribute__("sealed")
+        return f"Component({conf=}, {runtime=}, {sealed=})"
 
     def get_non_null_members_as_dict(self):
         resolved_conf = OmegaConf.to_container(
@@ -79,11 +78,3 @@ class Component(Generic[T]):
         runtime = super().__getattribute__("runtime")
         result |= {k: v for k, v in runtime.__dict__.items() if v is not None}
         return result
-
-
-def get_conf_as_dict(obj: object):
-    return {
-        name: obj.__getattribute__(name)
-        for name in obj.__annotations__
-        if obj.__getattribute__(name) is not None
-    }
